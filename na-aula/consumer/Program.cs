038ef7ec1228e41f0a64c90a5b1bcaf3fcfb4126
eduardoworrel <eduardo.worrel@gmail.com>
2022-06-28 22:02:14 -0300

    durante a aula

---
+++ na-aula/consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { 
+    HostName = "143.244.137.227",
+    UserName = "admin",
+    Password = "devInRabbit"
+    };
+
+using(var connection = factory.CreateConnection()) //disposable
+using(var channel = connection.CreateModel())
+{
+    var consumerText = new EventingBasicConsumer(channel);
+    var consumerNum = new EventingBasicConsumer(channel);
+    var consumerBool = new EventingBasicConsumer(channel);
+    
+    consumerText.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [Texto] recebido {0}", message);
+    };
+   
+    channel.BasicConsume(queue: "textos",
+                            autoAck: true,
+                            consumer: consumerText);
+
+
+    consumerNum.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [Numeros] recebido {0}", message);
+    };
+   
+    channel.BasicConsume(queue: "numeros",
+                            autoAck: true,
+                            consumer: consumerNum);
+
+    consumerBool.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [Booleano] recebido {0}", message);
+    };
+   
+    channel.BasicConsume(queue: "boleanos",
+                            autoAck: true,
+                            consumer: consumerBool);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ na-aula/publisher/Program.cs
@@ -0,0 +1,77 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { 
+    HostName = "143.244.137.227",
+    UserName = "admin",
+    Password = "devInRabbit"
+    };
+
+using(var connection = factory.CreateConnection()) //disposable
+using(var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare("multiple-ew", ExchangeType.Direct);
+
+    channel.QueueDeclare(queue: "textos",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+    channel.QueueBind(queue: "textos",
+            exchange: "multiple-ew",
+            routingKey: "textos"); 
+
+    channel.QueueDeclare(queue: "numeros",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+    channel.QueueBind(queue: "numeros",
+            exchange: "multiple-ew",
+            routingKey: "numeros"); 
+
+    channel.QueueDeclare(queue: "boleanos",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+    channel.QueueBind(queue: "boleanos",
+            exchange: "multiple-ew",
+            routingKey: "boleanos"); 
+   
+
+    string messageTexto = "Hello World!";
+    
+    string messageNumber = "123456789";
+    
+    string messageBoleana = "true";
+    
+    
+
+    channel.BasicPublish(exchange: "multiple-ew",
+                        routingKey: "textos",
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes(messageTexto));
+
+    Console.WriteLine(" [x] Sent {0}", messageTexto);
+
+    channel.BasicPublish(exchange: "multiple-ew",
+                        routingKey: "numeros",
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes(messageNumber));
+
+    Console.WriteLine(" [x] Sent {0}", messageNumber);
+
+    channel.BasicPublish(exchange: "multiple-ew",
+                        routingKey: "boleanos",
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes(messageBoleana));
+
+
+    Console.WriteLine(" [x] Sent {0}", messageBoleana);
+
+
+}
